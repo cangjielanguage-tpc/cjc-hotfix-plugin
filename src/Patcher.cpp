4452fdbc4f1594282823ea584f71d2044db09a56
ilyas <selimov.ilyas1@huawei.com>
2026-08-20 19:17:45 +0700

    test fix after rebasing to fresh main

--- src/Patcher.cpp
+++ src/Patcher.cpp
@@ -543,7 +543,6 @@ ClassDef* Patcher::genGuardClass() const
  */
 Function* Patcher::genGuardVarsInitializer() const
 {
-    std::vector<Type*> paramTypes;
     const auto gviName = PatchableName(package).genGuardVarInitializedName();
     const auto gvi = createEmptyFunctionWithUnitRetVal(gviName, {});
 
@@ -1064,7 +1063,8 @@ void Patcher::genShouldNotReachHere(Block* block) const
     CHIR::CreateAndAppendTerminator<RaiseException>(builder, allocException->GetResult(), block);
 }
 
-Function* Patcher::createEmptyFunctionWithUnitRetVal(const std::string& name, std::vector<Type*>& paramTypes) const
+Function* Patcher::createEmptyFunctionWithUnitRetVal(
+    const std::string& name, const std::vector<Type*>& paramTypes) const
 {
     const auto fType = builder.GetType<FuncType>(paramTypes, builder.GetUnitTy());
     const auto f = builder.CreateFunction(fType, name, name, name, package->GetName(), {});
